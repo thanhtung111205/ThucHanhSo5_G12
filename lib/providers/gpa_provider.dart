e037dc3e40754fe67517e9694e291fb111e5ef28
DangHuy1302 <lphan7024@gmail.com>
2026-03-17 22:45:14 +0700

    feat: hoan thanh task 2 chi tiet sinh vien

--- lib/providers/gpa_provider.dart
+++ lib/providers/gpa_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ThucHanhSo5_G12/models/subject.dart';
 
 class GpaProvider with ChangeNotifier {
   // State management for subject list and GPA calculation logic
